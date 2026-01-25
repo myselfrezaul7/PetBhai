f47b12c24ca56c430a869b9ce843b371d62b523b
shawono7 <myselfrezaul@outlook.com>
2026-01-25 20:54:37 +0100

    Clean up debug code, restore production settings

--- index.tsx
+++ index.tsx
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
 
-console.log('[PetBhai] Starting app initialization...');
-
-const BUILD_VERSION = '2026-01-25-v3';
 const rootElement = document.getElementById('root');
-console.log('[PetBhai] Root element found:', !!rootElement);
-
 if (!rootElement) {
-  throw new Error(`Could not find root element - build ${BUILD_VERSION}`);
+  throw new Error('Could not find root element to mount to');
 }
 
-console.log('[PetBhai] Creating React root...');
 const root = ReactDOM.createRoot(rootElement);
-
-console.log('[PetBhai] Rendering app...');
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
@@ -24,4 +16,3 @@ root.render(
     </ErrorBoundary>
   </React.StrictMode>
 );
-console.log('[PetBhai] Render called successfully');
